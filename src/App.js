cb3e257fc6728dfdddea144e7e48a0a2bc7727b1
Ave Nurme <nurme.ave@gmail.com>
2022-06-14 11:41:17 +0300

    add quiz folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Header from './header/Header';
 import Footer from './footer/Footer';
+import Quiz from './quiz/Quiz';
 
 function App() {
   return (
     <main className="main-container">
       <Header />
+      <Quiz />
       <Footer />
     </main>
   );
---
+++ src/quiz/Quiz.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../quiz/Quiz.css';
+
+function Quiz() {
+  return <section>Quiz</section>;
+}
+
+export default Quiz;
