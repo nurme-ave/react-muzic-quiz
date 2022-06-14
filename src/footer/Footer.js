00af20e06825e00b72e37be9eb5228f06da3b456
Ave Nurme <nurme.ave@gmail.com>
2022-06-14 11:17:37 +0300

    finish Header, Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,7 +3,12 @@ import '../footer/Footer.css';
 
 function Footer() {
   const year = new Date().getFullYear();
-  return <footer className='footer'>&copy; Ave Nurme {year}</footer>;
+  return (
+    <footer className="footer">
+      &copy; {year}
+      <a href="https://www.avenurme.dev">Ave Nurme | avenurme.dev</a>
+    </footer>
+  );
 }
 
 export default Footer;
