2712135472d62aa68434dc95f0775bd57cfd14da
Ave Nurme <nurme.ave@gmail.com>
2022-06-14 19:42:21 +0300

    start working on the trivia elements

---
+++ src/quiz/TriviaElem.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import './TriviaElem.css';
+
+
+function TriviaElem() {
+  return <div>TriviaElem</div>;
+}
+
+export default TriviaElem;
