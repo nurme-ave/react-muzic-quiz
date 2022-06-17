110210473fca8e84965a5349b42e6676e71e50ca
Ave Nurme <nurme.ave@gmail.com>
2022-06-17 19:15:27 +0300

    add final-results block

--- src/quiz/QuizPage.js
+++ src/quiz/QuizPage.js
@@ -2,7 +2,11 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { nanoid } from 'nanoid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMusic, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faMusic,
+  faArrowRight,
+  faMedal,
+} from '@fortawesome/free-solid-svg-icons';
 import he from 'he';
 
 import '../quiz/QuizPage.css';
@@ -229,8 +233,16 @@ function QuizPage() {
           </motion.div>
         </AnimatePresence>
       ) : (
-        <div>
-          <p>Game Over</p>
+        <div className="final-results">
+          <h1>Quiz complete!</h1>
+          <h2>
+            Congrats!
+            <FontAwesomeIcon icon={faMedal} className="fa-icon" />
+          </h2>
+          <div className="end-screen-stats">
+            <div className="end-screen-stat-label">Final score</div>
+            <div className="end-screen-stat-value">10</div>
+          </div>
           <button className="trivia-card-next-button" onClick={restartGame}>
             Play again?
           </button>
