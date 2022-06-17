3a4e143bf1212378f289dd223540fade6bb2114c
Ave Nurme <nurme.ave@gmail.com>
2022-06-17 08:34:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './index.css';
 import Header from './header/Header';
-import Quiz from './quiz/Quiz';
 import Footer from './footer/Footer';
 
 function App() {
   return (
     <main className="main-container">
       <Header />
-      <Quiz />
       <Footer />
     </main>
   );
