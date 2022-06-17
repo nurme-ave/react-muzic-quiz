e19551201e54e25d34edab5c0b5da20b09a1e912
Ave Nurme <nurme.ave@gmail.com>
2022-06-17 10:38:22 +0300

    make options in selection tags dynamic with map()

--- src/quiz/QuizPage.js
+++ src/quiz/QuizPage.js
@@ -2,23 +2,38 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
-
 // import he from "he";
+
 import '../quiz/QuizPage.css';
 
 function QuizPage() {
   const [difficultyLevel, setDifficultyLevel] = useState('');
   const [numOfQuestions, setNumOfQuestions] = useState(0);
   const [userSelections, setUserSelections] = useState({
-    difficulty: '',
-    numOfQuestions: 0,
+    selectionDifficulty: '',
+    selectionQuestions: 0,
   });
 
   console.log(difficultyLevel)
   console.log(numOfQuestions)
+  console.log(userSelections)
 
-  function getUserSelections() {
-    return;
+  const arrDifficultyLevels = ['Easy', 'Medium', 'Hard']
+  const optionDifficultyLevels = arrDifficultyLevels.map((level) => {
+    return <option key={level.toLowerCase()} value={level.toLowerCase()}>{level}</option>
+  })
+
+  const arrNumOfQuestions = ['5', '10', '15']
+  const optionNumOfQuestions = arrNumOfQuestions.map((num) => {
+    return <option key={num} value={+num}>{num}</option>
+  })
+
+  function getUserSelections(e) {
+    e.preventDefault();
+    setUserSelections({
+      selectionDifficulty: difficultyLevel,
+      selectionQuestions: +numOfQuestions,
+    });
   }
 
   return (
@@ -40,6 +55,7 @@ function QuizPage() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.5, duration: 1 }}
+        onSubmit={getUserSelections}
       >
         <div className="select-container">
           <label htmlFor="difficulty">Difficulty level:</label>
@@ -49,9 +65,7 @@ function QuizPage() {
             onChange={(e) => setDifficultyLevel(e.target.value)}
           >
             <option value="">-- Select --</option>
-            <option value="Easy">Easy</option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
+            {optionDifficultyLevels}
           </select>
         </div>
         <div className="select-container">
@@ -62,12 +76,10 @@ function QuizPage() {
             onChange={(e) => setNumOfQuestions(e.target.value)}
           >
             <option value="">-- Select --</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
+            {optionNumOfQuestions}
           </select>
         </div>
-        <button onClick={getUserSelections} className="start-quiz-button">
+        <button className="start-quiz-button">
           Start!
         </button>
       </motion.form>
