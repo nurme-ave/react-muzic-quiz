302846d410ed8dc6f7b8abfe3c451cd3b9747d31
Ave Nurme <nurme.ave@gmail.com>
2022-06-19 09:54:22 +0300

    fix style issues

--- src/quiz/QuizPage.js
+++ src/quiz/QuizPage.js
@@ -149,62 +149,6 @@ function QuizPage() {
       animate={{ opacity: 1 }}
       transition={{ delay: 1, duration: 1 }}
     >
-      {showIntro && (
-        <div>
-          <h2>Welcome!</h2>
-          <p>Make your pick below and start the quiz.</p>
-          <p>
-            Good luck!
-            <FontAwesomeIcon icon={faMusic} className="fa-icon" />
-          </p>
-        </div>
-      )}
-
-      {/* { !isGameOver && showIntro && <motion.form
-        className="form-container"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 1 }}
-        onSubmit={startTheGame}
-      > 
-        <div className="select-container">
-          <label htmlFor="difficulty">Difficulty level:</label>
-          <select
-            value={difficultyLevel}
-            id="difficulty"
-            onChange={(e) => setDifficultyLevel(e.target.value)}
-          >
-            <option value="">-- Select --</option>
-            {optionDifficultyLevels}
-          </select>
-        </div>
-        <div className="select-container">
-          <label htmlFor="numOfQuestions">Number of questions:</label>
-          <select
-            value={numOfQuestions}
-            id="numOfQuestions"
-            onChange={(e) => setNumOfQuestions(e.target.value)}
-          >
-            <option value="">-- Select --</option>
-            {optionNumOfQuestions}
-          </select>
-        </div>
-        <button className="start-quiz-button">Start!</button>
-      </motion.form>} */}
-
-      {/* { showGame ? (
-      <div>
-        <Stats quizData={quizData} />
-        <TriviaCard quizData={quizData} onloadNextClick={loadNextQuestion} onFinishClick={finishQuiz} />
-      </div>
-      ) : (
-        <EndScreen onRestartClick={restartGame}/> ?
-      ) : (
-
-      )
-      
-      } */}
-
       {showGame ? (
         <div>
           <Stats quizData={quizData} />
@@ -217,48 +161,48 @@ function QuizPage() {
       ) : isGameOver ? (
         <EndScreen onRestartClick={restartGame} />
       ) : (
-        <motion.form
-          className="form-container"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 1 }}
-          onSubmit={startTheGame}
-        >
-          <div className="select-container">
-            <label htmlFor="difficulty">Difficulty level:</label>
-            <select
-              value={difficultyLevel}
-              id="difficulty"
-              onChange={(e) => setDifficultyLevel(e.target.value)}
-            >
-              <option value="">-- Select --</option>
-              {optionDifficultyLevels}
-            </select>
+        <>
+          <div className="intro">
+            <h2>Welcome!</h2>
+            <p>Make your pick below and start the quiz.</p>
+            <p>
+              Good luck!
+              <FontAwesomeIcon icon={faMusic} className="fa-icon" />
+            </p>
           </div>
-          <div className="select-container">
-            <label htmlFor="numOfQuestions">Number of questions:</label>
-            <select
-              value={numOfQuestions}
-              id="numOfQuestions"
-              onChange={(e) => setNumOfQuestions(e.target.value)}
-            >
-              <option value="">-- Select --</option>
-              {optionNumOfQuestions}
-            </select>
-          </div>
-          <button className="start-quiz-button">Start!</button>
-        </motion.form>
+          <motion.form
+            className="form-container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 1 }}
+            onSubmit={startTheGame}
+          >
+            <div className="select-container">
+              <label htmlFor="difficulty">Difficulty level:</label>
+              <select
+                value={difficultyLevel}
+                id="difficulty"
+                onChange={(e) => setDifficultyLevel(e.target.value)}
+              >
+                <option value="">-- Select --</option>
+                {optionDifficultyLevels}
+              </select>
+            </div>
+            <div className="select-container">
+              <label htmlFor="numOfQuestions">Number of questions:</label>
+              <select
+                value={numOfQuestions}
+                id="numOfQuestions"
+                onChange={(e) => setNumOfQuestions(e.target.value)}
+              >
+                <option value="">-- Select --</option>
+                {optionNumOfQuestions}
+              </select>
+            </div>
+            <button className="start-quiz-button">Start!</button>
+          </motion.form>
+        </>
       )}
-
-      {/* 
-      { showGame ? (
-      <div>
-        <Stats quizData={quizData} />
-        <TriviaCard quizData={quizData} onloadNextClick={loadNextQuestion} onFinishClick={finishQuiz} />
-      </div>
-      ) : (
-        <EndScreen onRestartClick={restartGame}/>
-      )} */}
     </motion.section>
   );
 }
