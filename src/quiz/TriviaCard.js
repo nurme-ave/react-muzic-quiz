29553b38e3073261a4f2744352fddc87b94bcfb9
Ave Nurme <nurme.ave@gmail.com>
2022-06-23 18:56:49 +0300

    disable 'Finish Quiz' button when !haspickedAnswer

--- src/quiz/TriviaCard.js
+++ src/quiz/TriviaCard.js
@@ -7,11 +7,7 @@ import he from 'he';
 import '../quiz/TriviaCard.css';
 import Spinner from './Spinner';
 
-function TriviaCard({
-  quizData,
-  onloadNextClick,
-  onFinishClick,
-}) {
+function TriviaCard({ quizData, onloadNextClick, onFinishClick }) {
   const { questionIndex } = useContext(QuizContext);
   const { selectedAnswer, setSelectedAnswer } = useContext(QuizContext);
   const { score, setScore } = useContext(QuizContext);
@@ -22,7 +18,7 @@ function TriviaCard({
 
   useEffect(() => {
     setLoaded(true);
-  }, [quizData])
+  }, [quizData]);
 
   const haspickedAnswer = selectedAnswer !== null;
 
@@ -41,66 +37,70 @@ function TriviaCard({
 
   return (
     <>
-    <AnimatePresence exitBeforeEnter={true}>
-      <motion.div
-        className="trivia-card-container"
-        key={questionIndex}
-        initial={{ opacity: 0, x: -300, transition: { ease: 'easeOut' } }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: 300 }}
-        transition={{ duration: 1 }}
-      >
-        <p className="trivia-card-question">
-          {quizData[questionIndex].question}
-        </p>
-        <ul className="trivia-card-answers">
-          {quizData[questionIndex].allAnswers.map((answer) => {
-            let answerButtonClasses = 'trivia-card-button';
+      <AnimatePresence exitBeforeEnter={true}>
+        <motion.div
+          className="trivia-card-container"
+          key={questionIndex}
+          initial={{ opacity: 0, x: -300, transition: { ease: 'easeOut' } }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 300 }}
+          transition={{ duration: 1 }}
+        >
+          <p className="trivia-card-question">
+            {quizData[questionIndex].question}
+          </p>
+          <ul className="trivia-card-answers">
+            {quizData[questionIndex].allAnswers.map((answer) => {
+              let answerButtonClasses = 'trivia-card-button';
 
-            if (haspickedAnswer) {
-              const pickedThisAnswer = answer === selectedAnswer;
-              const isThisCorrect =
-                answer === quizData[questionIndex].correctAnswer;
+              if (haspickedAnswer) {
+                const pickedThisAnswer = answer === selectedAnswer;
+                const isThisCorrect =
+                  answer === quizData[questionIndex].correctAnswer;
 
-              if (pickedThisAnswer && isThisCorrect) {
-                answerButtonClasses += ' trivia-card-button-correct';
-              } else if (pickedThisAnswer && !isThisCorrect) {
-                answerButtonClasses += ' trivia-card-button-incorrect';
-              } else {
-                answerButtonClasses += ' trivia-card-button-disabled';
+                if (pickedThisAnswer && isThisCorrect) {
+                  answerButtonClasses += ' trivia-card-button-correct';
+                } else if (pickedThisAnswer && !isThisCorrect) {
+                  answerButtonClasses += ' trivia-card-button-incorrect';
+                } else {
+                  answerButtonClasses += ' trivia-card-button-disabled';
+                }
               }
-            }
 
-            return (
-              <li key={answer}>
-                <button
-                  className={answerButtonClasses}
-                  onClick={onAnswerClick}
-                  disabled={haspickedAnswer}
-                >
-                  {he.decode(answer)}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-        {questionIndex >= quizData.length - 1 ? (
-          <button className="trivia-card-next-button" onClick={onFinishClick}>
-            Finish quiz
-            <FontAwesomeIcon icon={faArrowRight} className="fa-icon-arrow" />
-          </button>
-        ) : (
-          <button
-            className="trivia-card-next-button"
-            onClick={onloadNextClick}
-            disabled={!haspickedAnswer}
-          >
-            Next
-            <FontAwesomeIcon icon={faArrowRight} className="fa-icon-arrow" />
-          </button>
-        )}
-      </motion.div>
-    </AnimatePresence>
+              return (
+                <li key={answer}>
+                  <button
+                    className={answerButtonClasses}
+                    onClick={onAnswerClick}
+                    disabled={haspickedAnswer}
+                  >
+                    {he.decode(answer)}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          {questionIndex >= quizData.length - 1 ? (
+            <button 
+              className="trivia-card-next-button" 
+              onClick={onFinishClick}
+              disabled={!haspickedAnswer}
+              >
+              Finish quiz
+              <FontAwesomeIcon icon={faArrowRight} className="fa-icon-arrow" />
+            </button>
+          ) : (
+            <button
+              className="trivia-card-next-button"
+              onClick={onloadNextClick}
+              disabled={!haspickedAnswer}
+            >
+              Next
+              <FontAwesomeIcon icon={faArrowRight} className="fa-icon-arrow" />
+            </button>
+          )}
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
